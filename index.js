8c1c362b93ed1347cbc6b3b1ce3b000913aa4d01
Julen37 <julen.gouchault@gmail.com>
2025-07-08 14:45:17 +0200

    update caroussel fonctionnel

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ const arrayImg = [
 let imgSection = document.getElementById("imgCaroussel");
 let btnSection = document.getElementById("btnCaroussel");
 
-index = 0;
+let index = 0;
 imgSection.src = arrayImg[index]; //mettre la source de l'image le premier index de base du tableau pour quelle s'affiche en premier
 
 btnSection.onclick = function(){
-    imgSection.src = arrayImg[index + 1] ;
-    return index += 1; // pour mettre la prochaine image dans la src 
-        //  % arrayImg.length; //pour que ca se remette a 0 a la fin quand ca arrive a la longueur du tableau
-    
+    index = (index + 1) % arrayImg.length; //modulo de la longueur du tableau pour que ca se remette a 0 a la fin quand ca arrive a la longueur du tableau
+    setTimeout(() => { //defini un minuteur sur le code dedans
+        imgSection.src = arrayImg[index]; // Change la source de l'image
+    }, 200); // le temps que ca prend pour passer d'une image a une autre
+    // ca met un temps de 200 avant que ca change la source
 };
 
 //je veux prendre l'emplacement de l'image
 //pour pouvoir mettre la premiere valeur du tableau
 // dans l url de l'image 
 // quand on clic sur le bouton
+// et affiche les prochaine des qu'on appui
+
+//ameliorer pour mettre un bouton en plus pour envoyer au livre precedent
